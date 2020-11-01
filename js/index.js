07ecc534cd8865712b16be028cbbf3214cffd1ca
Olexandr-S <sasha-skidan@mail.ru>
2020-11-01 21:14:46 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:2,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[5000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
